7d95fd635984208dfeec6489ebc46f8ecbd38c5f
Ben Guinaudueau <bguinaudeau@gmail.com>
2020-11-27 22:22:00 -0500

    update dockerfile

--- sign.py
+++ sign.py
@@ -3,5 +3,5 @@ import sys
 from TikTokApi import TikTokApi
 
 api = TikTokApi.get_instance()
-res = api.browser.sign_url(url = sys.argv[1])
+res = api.browser.sign_url(url = sys.argv[1], custom_verifyFp = sys.argv[2])
 print(json.dumps(res))
